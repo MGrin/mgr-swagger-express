e84a917e0b7324b12a2a5e07e37b3302a3b6f663
MGrin <mr6r1n@gmail.com>
2019-12-07 20:06:29 +0000

    move all stuff to index

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,6 @@ import {
   transformBody2Swagger,
   transformQuery2Swagger,
 } from './utils';
-import {
-  SwaggerEndpoint,
-  SwaggerConfig,
-  SwaggerParameter,
-  Context,
-} from './types'
 
 let app: any
 const paths: any = {}
@@ -144,4 +138,67 @@ export const addSwaggerDefinition = (name: string, definition: object) => {
     throw new Error(`${name} model has been already defined`)
   }
   definitions[name] = definition
+}
+
+export type ID = string
+
+export type Context = {
+  author: ID
+  organization: ID
+  roles: string[]
+}
+
+export type Error = {
+  code: number
+  message: string
+}
+
+export type SwaggerURLParameter = {
+  name: string
+  description: string
+  type?: string
+}
+export type SwaggerBodyParameter = {
+  name: string
+  description: string
+  required: boolean
+  schema?: any
+  type: string
+  items?: object
+}
+export type SwaggerQueryField = {
+  name: string
+  type: string
+  required?: boolean
+  items?: object
+}
+export type SwaggerQueryParameter = {
+  name: string,
+  description: string,
+  items: SwaggerQueryField[]
+}
+export type SwaggerParameter = {
+  in?: string
+  example?: any
+} & (SwaggerURLParameter | SwaggerBodyParameter | SwaggerQueryParameter)
+
+export type SwaggerSuccessResponse = string
+
+export type SwaggerEndpoint = {
+  path: string
+  auth?: string
+  description?: string
+  tags?: string[]
+  parameters?: SwaggerURLParameter[]
+  query?: SwaggerQueryParameter
+  body?: SwaggerBodyParameter
+  success?: SwaggerSuccessResponse
+}
+
+export type SwaggerConfig = {
+  version: string
+  name: string
+  description: string
+  host: string
+  basePath: string
 }
